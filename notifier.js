205357c92a10f6b4148126601b07348daf140688
Hao Tran <hhoot25@gmail.com>
2025-10-13 18:11:09 -0500

    added dependencies

--- notifier.js
+++ notifier.js
@@ -1,1 +1,4 @@
 //this will be the node app that will send info
+import express from "express";
+import fetch from "node-fetch"; //send and receive http requests
+import querystring from "querystring"; //format url
